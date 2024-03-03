0e00dea0f800f1b11d3a6ceda60c651c835277bc
SharoonRafeek <sharoonrafeek@gmail.com>
2024-03-03 19:58:58 +0530

    Update port variable in app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 dotenv.config();
